8d8412a1798214ffb5187d0a1cb2e496b64f7687
Henrique Hott <cs.henriquemchott@gmail.com>
2024-03-24 20:53:05 -0300

    FIX: Error on __init__.py file when importing after installing using pip install +git.

--- nature/genetic/base/__init__.py
+++ nature/genetic/base/__init__.py
@@ -1,9 +1,9 @@
-from genetic.base.GeneticAlgorithmBase import GeneticAlgorithmBase
-from genetic.base.SelectionStrategyBase import SelectionStrategyBase
-from genetic.base.CrossoverStrategyBase import CrossoverStrategyBase
-from genetic.base.MutationStrategyBase import MutationStrategyBase
-from genetic.base.ElitismStrategyBase import ElitismStrategyBase
-from genetic.base.GeneticAlgorithmLoggerBase import GeneticAlgorithmLoggerBase
+from nature.genetic.base.GeneticAlgorithmBase import GeneticAlgorithmBase
+from nature.genetic.base.SelectionStrategyBase import SelectionStrategyBase
+from nature.genetic.base.CrossoverStrategyBase import CrossoverStrategyBase
+from nature.genetic.base.MutationStrategyBase import MutationStrategyBase
+from nature.genetic.base.ElitismStrategyBase import ElitismStrategyBase
+from nature.genetic.base.GeneticAlgorithmLoggerBase import GeneticAlgorithmLoggerBase
 
 __all__ = [
     "GeneticAlgorithmBase",
